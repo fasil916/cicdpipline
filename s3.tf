7a2bf17094479c30bc43855f6baba31392db3ac3
fasil916 <71936145+fasil916@users.noreply.github.com>
2023-11-24 11:32:46 +0530

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "fazzzzz"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
