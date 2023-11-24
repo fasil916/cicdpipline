336a61d38d37fcdef106415e94c30d9618bb3c74
fasil916 <71936145+fasil916@users.noreply.github.com>
2023-11-24 13:25:31 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,8 @@
 provider "aws" {
-  region = "us-east-1"
+   region     = "us-east-1"
+    access_key = "AKIA2O2PFYHDCXHL774Q"
+    
+    secret_key = "y3QCZGDLHqfEJGxV4RoLQZNG/HRqzIdTophHari3"
 }
 
 resource "aws_s3_bucket" "example" {
